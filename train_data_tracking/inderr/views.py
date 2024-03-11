02e48bb556eb00e79d959262989b9f79a25220c5
AmanGupta1542 <amangupta1542@gmail.com>
2024-03-11 18:48:22 +0530

    new changes

--- train_data_tracking/inderr/views.py
+++ train_data_tracking/inderr/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 import json
+import pickle
+import sys
 from django.db.models import Q
+from datetime import datetime, time
 
 from .forms import UserLogin, UserImage
 from .models import Trains, TrainInnerStation, UploadImage, Temp
@@ -70,6 +73,55 @@ def trains(request):
     else :
         return redirect('/')
 
+def emit_rpi_data(request, data):
+    import socket
+    # Define the Raspberry Pi's IP address and port
+    # raspberry_pi_ip = '192.168.43.15'  # aman wifi
+    #raspberry_pi_ip = '192.168.137.8'
+    raspberry_pi_ip = '192.168.31.89'  # madhya airtel
+    raspberry_pi_port = 1026
+    try:
+        print('try connecting')
+        print('final data')
+        print(data)
+        # print(request.session.get('train_stations'))
+        train_stations = request.session.get('train_stations')
+        train_stations['next_station'] = data['next_station']
+        train_stations['curr_location'] = data['curr_location']
+        train_stations['speed'] = data['speed']
+        train_stations['late_by'] = data['late_by']
+        print(train_stations)
+        # Create a socket object
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to the Raspberry Pi
+        client_socket.connect((raspberry_pi_ip, raspberry_pi_port))
+
+        # Send data to the Raspberry Pi
+        # data = "Hello, Raspberry Pi!"
+        # data = data['next_stations']
+        print('final data')
+        
+        # train_stations = request.session.get('train_stations')
+        # data = train_stations.update(data)
+        # print(data)
+        print("Size of data_list:", sys.getsizeof(train_stations), "bytes")
+        # Serialize the dictionary object using pickle
+        serialized_data = pickle.dumps(train_stations)
+        # serialized_data = json.dumps(data).encode('utf-8')
+        client_socket.send(serialized_data)
+        # Close the socket
+        client_socket.close()
+        return JsonResponse({'success': 'Called Successfully'}, status=200)
+    except socket.gaierror as e:
+        print({'error': f"An error occurred: {e}"})
+        return JsonResponse({'error': f"An error occurred: {e}"}, status=500)
+        # return f"Socket error: {e}"
+    except Exception as e:
+        print({'error': f"An error occurred: {e}"})
+        return JsonResponse({'error': f"An error occurred: {e}"}, status=500)
+        # return f"An error occurred: {e}"
+
 def train_details(request, train_number):
     if request.user.is_authenticated:
         train = Trains.objects.get(number=train_number)
@@ -106,11 +158,169 @@ def train_details(request, train_number):
         #         next_stations = [stat for stat in sorted_stations if min_stat['order']+1 == stat['order']][0]
         #     else :
         #         next_stations = min_stat
-
-
-        context = {'train': train, 'object_list':object_list, 'places':places, 'curr_location':get_coords(), 'next_stations':next_stations}
-        # print(object_list)
+        current_time = datetime.now().time()
+        # Convert the current time to a datetime object with today's date
+        current_datetime = datetime.combine(datetime.today(), current_time)
+        late_by_min = (current_datetime - datetime.combine(datetime.today(), object_list[0].departs)).total_seconds() / 60
+        late_by = 0 if late_by_min < 0 else round(late_by_min, 0)
+        context = {'train': train, 'object_list':object_list, 'places':places, 'curr_location':get_coords(), 'next_stations':next_stations }
+        print("emit_rpi_data call")
+        stations = []
+        for station in object_list:
+            stations.append({
+                'name': station.station_id.name,
+                'abbr': station.station_id.code,
+                'distance': station.distance,
+                'order': station.order,
+                'estimate_time': station.arrives.strftime('%H:%M') if station.arrives is not None else None,
+                'delay_time': station.departs.strftime('%H:%M') if station.departs is not None else None,
+                'lat': str(station.station_id.lat),
+                'lon': str(station.station_id.lon),
+                'halt_time': station.halt_time
+            })
+        res_data = {
+            'train': {
+                'name': train.name,
+                'number': train.number,
+                'from_station': train.from_station.name,
+                'to_station': train.to_station.name
+            },
+            'stations': stations,
+            # 'next_station':next_stations,
+            # 'curr_location':get_coords(),
+            # 'speed': 0,
+            # 'late_by': late_by
+        }
+        # print(res_data)
+        # serialized_data = pickle.dumps(res_data)
+        # print('data serialized')
+        # print(serialized_data)
+        # deserialized_data = pickle.loads(serialized_data, encoding='utf-8')
+        # print('deserialized data')
+        # print(deserialized_data)
+        # emit_rpi_data(request, res_data)
+        request.session['train_stations'] = res_data
         return render(request, 'inderr/train-details.html', context)
+        context = {
+            # 'train': {
+            #     'id': 3,
+            #     'name': 'rj',
+            #     'number': 12161,
+            #     'inserted_at': '',
+            #     'from_station': {
+            #         'name': 'Bhopal', 
+            #         'lat': Decimal('24.172716'), 
+            #         'lon': Decimal('78.184876'), 
+            #         'code': 'BPL', 
+            #         'distance': 10.97408434219449
+            #     },
+            #     'to_station': {
+            #         'name': 'Damoh', 
+            #         'lat': Decimal('24.172716'), 
+            #         'lon': Decimal('78.184876'), 
+            #         'code': "DMO", 
+            #     },
+            #     'monday': true,
+            #     'tuesday': false,
+            # },
+            # 'object_list': [
+            #     {
+            #         'id': 8,
+            #         'order': 1,
+            #         'inserted_at': '',
+            #         'station_id': {
+            #             'name': 'Bhopal', 
+            #             'lat': Decimal('24.172716'), 
+            #             'lon': Decimal('78.184876'), 
+            #             'code': 'BPL',
+            #         },
+            #         'train_id': {
+            #             'id': 3,
+            #             'name': 'rj',
+            #             'number': 12161,
+            #             'inserted_at': '',
+            #             'from_station': {
+            #                 'name': 'Bhopal', 
+            #                 'lat': Decimal('24.172716'), 
+            #                 'lon': Decimal('78.184876'), 
+            #                 'order': 1, 
+            #                 'distance': 10.97408434219449
+            #             },
+            #             'to_station': {
+            #                 'name': 'Damoh', 
+            #                 'lat': Decimal('24.172716'), 
+            #                 'lon': Decimal('78.184876'), 
+            #                 'order': 12, 
+            #                 'distance': 10.97408434219449
+            #             },
+            #             'monday': true,
+            #             'tuesday': false,
+            #         },
+            #         'arrives': 44,
+            #         'avg_delay': 2,
+            #         'day': 1,
+            #         'departs': 12.40,
+            #         'distance': '30km',
+            #         'halt_time': "2 min",
+            #         "is_stop": True
+            #     },
+            #     {
+            #         'id': 7,
+            #         'order': 2,
+            #         'inserted_at': '',
+            #         'station_id': {
+            #             'name': 'Vidisha', 
+            #             'lat': Decimal('24.172716'), 
+            #             'lon': Decimal('78.184876'),
+            #             'code': 'VHS'
+            #         },
+            #         'train_id': {
+            #             'id': 3,
+            #             'name': 'rj',
+            #             'number': 12161,
+            #             'inserted_at': '',
+            #             'from_station': {
+            #                 'name': 'Bhopal', 
+            #                 'lat': Decimal('24.172716'), 
+            #                 'lon': Decimal('78.184876'), 
+            #                 'order': 1, 
+            #                 'distance': 10.97408434219449
+            #             },
+            #             'to_station': {
+            #                 'name': 'Damoh', 
+            #                 'lat': Decimal('24.172716'), 
+            #                 'lon': Decimal('78.184876'), 
+            #                 'order': 12, 
+            #                 'distance': 10.97408434219449
+            #             },
+            #             'monday': true,
+            #             'tuesday': false,
+            #         },
+            #         'arrives': 44,
+            #         'avg_delay': 2,
+            #         'day': 1,
+            #         'departs': 12.40,
+            #         'distance': '30km',
+            #         'halt_time': "2 min",
+            #         "is_stop": True
+            #     },
+                
+            # ],
+            # 'curr_location': {
+            #     'lat': Decimal('24.115625'), 
+            #     'lon': Decimal('78.111797')
+            # },
+            # 'next_stations': {
+            #     'name': 'Bina', 
+            #     'lat': Decimal('24.172716'), 
+            #     'lon': Decimal('78.184876'), 
+            #     'order': 5, 
+            #     'distance': 10.97408434219449
+            # },
+            # 'speed': 0,
+            # 'late_by': 3
+        }
+        
     else :
         return redirect('/')
 
@@ -207,32 +417,41 @@ def send_data_rsp(request):
         import socket
         data = json.load(request)['data']
         print(data)
-        # Define the Raspberry Pi's IP address and port
-        # raspberry_pi_ip = '192.168.43.15'  # aman wifi
-        #raspberry_pi_ip = '192.168.137.8'
-        raspberry_pi_ip = '192.168.1.4'  # madhya airtel
-        raspberry_pi_port = 1026
-        try:
-            # Create a socket object
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 'next_station':next_stations,
+        # 'curr_location':get_coords(),
+        # 'speed': 0,
+        # 'late_by': late_by
+        data['curr_location'] = get_coords()
+        data['speed'] = 0
+        data['late_by'] = 30
+        print(data)
+        emit_rpi_data(request, data)
+        # # Define the Raspberry Pi's IP address and port
+        # # raspberry_pi_ip = '192.168.43.15'  # aman wifi
+        # #raspberry_pi_ip = '192.168.137.8'
+        # raspberry_pi_ip = '192.168.31.89'  # madhya airtel
+        # raspberry_pi_port = 1026
+        # try:
+        #     # Create a socket object
+        #     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-            # Connect to the Raspberry Pi
-            client_socket.connect((raspberry_pi_ip, raspberry_pi_port))
+        #     # Connect to the Raspberry Pi
+        #     client_socket.connect((raspberry_pi_ip, raspberry_pi_port))
 
-            # Send data to the Raspberry Pi
-            # data = "Hello, Raspberry Pi!"
-            data = data['next_stations']
-            print(data)
-            client_socket.send(data.encode('utf-8'))
-            # Close the socket
-            client_socket.close()
-            return JsonResponse({'success': 'Called Successfully'}, status=200)
-        except socket.gaierror as e:
-            return JsonResponse({'error': f"An error occurred: {e}"}, status=500)
-            # return f"Socket error: {e}"
-        except Exception as e:
-            return JsonResponse({'error': f"An error occurred: {e}"}, status=500)
-            # return f"An error occurred: {e}"
+        #     # Send data to the Raspberry Pi
+        #     # data = "Hello, Raspberry Pi!"
+        #     data = data['next_stations']
+        #     print(data)
+        #     client_socket.send(data.encode('utf-8'))
+        #     # Close the socket
+        #     client_socket.close()
+        #     return JsonResponse({'success': 'Called Successfully'}, status=200)
+        # except socket.gaierror as e:
+        #     return JsonResponse({'error': f"An error occurred: {e}"}, status=500)
+        #     # return f"Socket error: {e}"
+        # except Exception as e:
+        #     return JsonResponse({'error': f"An error occurred: {e}"}, status=500)
+        #     # return f"An error occurred: {e}"
     return JsonResponse({'error': 'Method not allowed'}, status=403)
 
 def get_updated_info(request):
